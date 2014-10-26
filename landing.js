06783daca4d6a2d56cf44098d29e265da1bc1dc6
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-10-26 17:27:11 +0100

    landing page

---
+++ landing.js
@@ -0,0 +1,61 @@
+var express         = require('express');
+    path            = require('path'),
+    favicon         = require('static-favicon'),
+    logger          = require('morgan'),
+    cookieParser    = require('cookie-parser'),
+    bodyParser      = require('body-parser'),
+    fs              = require('fs'),
+    app             = express(),
+    db              = require('./db'),
+    secrets         = require('./config/secret-keys'),
+    config          = require('./config/config')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon())
+app.use(logger('dev'))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.use(require('stylus').middleware(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('/', function(req, res){
+  res.render('landing')
+})
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.redirect('/')
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
