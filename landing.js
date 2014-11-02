9240c86be3f21e2d9a42f2655352c92c7f394ded
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-11-02 17:50:46 +0100

    add background image

--- landing.js
+++ landing.js
@@ -44,7 +44,9 @@ app.post('/join', function(req, res){
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.redirect('/')
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // development error handler
